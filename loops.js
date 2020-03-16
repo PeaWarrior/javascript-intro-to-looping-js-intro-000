2a7cd1514d011bc0b7018a27a378e5c91c478569
Jackson Chen <jacksonchen171@gmail.com>
2020-03-16 12:38:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,6 +13,14 @@ function forLoop(array){
 function whileLoop(n){
   while (n !== 0){
     console.log(n)
+    n -= 1
   }
   return "done"
+}
+
+function doWhileLoop(num){
+  do {
+    console.log("I run once regardless.")
+    num -= 1
+  } while num > 0
 }
